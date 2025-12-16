d0f20a1ec27d8303d9869a5c5c76b66416e38bfb
lei_ji_ <94916764+leiji001@users.noreply.github.com>
2025-12-16 23:17:58 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -571,7 +571,7 @@ body::before {
   width: 100%;
   height: 100%;
   z-index: -1;
-  background: url(https://bing.biturl.top/?format=image&index=0&mkt=zh-CN) center/cover no-repeat;
+  background: url(//status.nianlei.icu/api/background) center/cover no-repeat;
   filter: blur(0px);
 }
 
